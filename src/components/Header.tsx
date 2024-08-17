621c651a0517e8d92f719f0abd66a67cf79561e4
Antonio880 <antoniocruzgomes880@gmail.com>
2024-08-17 00:15:04 -0300

    feat: implementação inicial do header e configuração de roteamento

---
+++ src/components/Header.tsx
@@ -0,0 +1,56 @@
+const links = [
+  {
+    label: "Início",
+    dropdown: ["Sobre", "Time"]
+  },
+  {
+    label: "RESET",
+    dropdown: ["Reset 1", "Reset 2"]
+  },
+  {
+    label: "Services",
+    dropdown: ["Web Development", "App Development"]
+  },
+  {
+    label: "Contact"
+  }
+];
+
+export default function Header() {
+  return (
+    <div className="fixed top-0 left-0 w-full z-50 bg-white shadow-lg">
+      <header className="flex px-16 justify-between items-center p-4">
+        <div>
+          <img
+            src="public/LOGO HORIZONTAL FT 2.png"
+            alt=""
+            className="w-[240px]"
+          />
+        </div>
+        <nav className="flex h-[60px] space-x-8 text-white">
+          {links.map((link, index) => (
+            <div key={index} className="relative group h-full">
+              <div className="transition h-full flex items-center cursor-pointer font-bold hover:text-[#ec642a]">
+                {link.label}
+              </div>
+              {link.dropdown && (
+                <div className="absolute left-0 w-[150px] hidden group-hover:block bg-white text-gray-800 rounded-lg p-5 shadow-lg">
+                  {link.dropdown.map((item, idx) => (
+                    <a
+                      key={idx}
+                      href="#"
+                      className="relative bg-white block py-1 text-gray-500 hover:text-gray-500 transition-all"
+                    >
+                      <span className="absolute left-0 top-1/2 transform -translate-y-1/2 w-0 h-0.5 bg-[#ec642a] transition-all duration-300 ease-out group-hover:w-4"></span>
+                      <span className="pl-5 bg-white">{item}</span>
+                    </a>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+        </nav>
+      </header>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import Header from './components/Header.tsx'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Header />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
