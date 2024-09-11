3a2978d0c0854092c490b2ee53f5c32aeb3407bc
Antonio880 <antoniocruzgomes880@gmail.com>
2024-09-11 09:42:51 -0300

    styles: mudança de font base

--- src/components/Collection/index.tsx
+++ src/components/Collection/index.tsx
@@ -1,5 +1,6 @@
 import CustomButton from "../CustomButton";
 import "./index.css";
+import foto from "../../assets/logo.png";
 
 export default function Collection() {
 
@@ -11,15 +12,15 @@ export default function Collection() {
   };
 
   return (
-    <div className="flex justify-center items-center animate-fade animate-once animate-alternate">
-      <div className="flex justify-evenly w-full h-[880px] items-center">
+    <div className="flex justify-center items-center animate-fade animate-once animate-alternate bg-[#270B79]">
+      <div className="flex justify-evenly w-full h-[890px] items-center">
         <div className="flex flex-col justify-center text-white">
-          <h1 className="text-8xl font-bold">RESETLAB</h1>
+          <h1 className="flex text-8xl font-Lufga-ExtraBold">Reset<h1 className="font-Lufga-Regular">Lab</h1></h1>
           <p className="text-4xl w-[350px] font-semibold my-7">Inovando na Conexão entre Software e Dados.</p>
           <CustomButton children="Venha nos Conhecer" onClick={handleScroll} />
         </div>
-        <div className="background text-white">
-          <img src="public/LOGO HORIZONTAL FT 2.png" width={700} alt="" />
+        <div className="background">
+          <img src={foto} width={700} alt="" />
         </div>
       </div>
     </div>
