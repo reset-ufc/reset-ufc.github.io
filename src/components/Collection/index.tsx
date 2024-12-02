3f2d8bdb12e36edc5e807f2357e8cfddbd25035f
ArthurWillameBr <arthurwillame2005@gmail.com>
2024-12-02 20:00:25 -0300

    feat: add animate

--- src/components/Collection/index.tsx
+++ src/components/Collection/index.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import CustomButton from "../CustomButton";
 import "./index.css";
 import vectorLeft from "/public/vetor branco.png";
@@ -21,56 +22,83 @@ export default function Collection() {
     }
   };
 
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: 0.5,
+        when: "beforeChildren",
+        staggerChildren: 0.3,
+      },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.3 },
+    },
+  };
+
   return (
     <div className="relative min-h-screen overflow-hidden bg-gradient-to-b from-[#270B79] to-[#1E0A5C]">
-      <div className="flex flex-col lg:flex-row items-center justify-center h-full px-4 mt-8  md:mt-32 lg:py-0 lg:px-16 max-w-7xl mx-auto">
-        <div className="flex flex-col mt-20 justify-center text-white text-center lg:text-left lg:w-1/2 space-y-6 mb-12 lg:mb-0">
-          <h1 className="text-4xl lg:text-6xl font-Lufga-ExtraBold leading-tight">
+      <motion.div
+        className="flex flex-col lg:flex-row items-center justify-center h-full px-4 mt-8 md:mt-32 lg:py-0 lg:px-16 max-w-7xl mx-auto"
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+      >
+        <motion.div
+          className="flex flex-col mt-20 justify-center text-white text-center lg:text-left lg:w-1/2 space-y-6 mb-12 lg:mb-0"
+          variants={containerVariants}
+        >
+          <motion.h1
+            className="text-4xl lg:text-6xl font-Lufga-ExtraBold leading-tight"
+            variants={itemVariants}
+          >
             Inovando na Conexão entre Software e Dados
-          </h1>
-          <p className="text-xl lg:text-2xl font-Lufga-Regular">
+          </motion.h1>
+          <motion.p
+            className="text-xl lg:text-2xl font-Lufga-Regular"
+            variants={itemVariants}
+          >
             Somos um laboratório de pesquisa e desenvolvimento da Universidade
             Federal do Ceará, Campus Itapajé
-          </p>
-          <div className="pt-4">
+          </motion.p>
+          <motion.div className="pt-4" variants={itemVariants}>
             <CustomButton
               onClick={handleScroll}
               className="text-lg px-8 py-3 w-full rounded-full transition-transform hover:scale-105"
             >
               Descubra Nossa Missão
             </CustomButton>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
 
         <div className="lg:w-1/2 flex justify-center items-center">
-          <div
-            className={`logo-container relative ${
-              animationFinished ? "merged" : ""
-            }`}
-          >
-            <img
+          <div className="logo-container relative">
+            <motion.img
               src={vectorLeft}
-              className="logo-part left-part w-32 md:w-48 lg:w-64 absolute top-0 left-0 transition-all duration-1000"
+              className="logo-part left-part w-32 md:w-48 lg:w-64 absolute top-0 left-0"
               alt="Símbolo de Inovação - Parte Esquerda"
-              style={{
-                transform: animationFinished
-                  ? "translateX(25%)"
-                  : "translateX(0)",
-              }}
+              initial={{ x: "-50%" }}
+              animate={{ x: animationFinished ? "0%" : "-50%" }}
+              transition={{ duration: 1, ease: "easeInOut" }}
             />
-            <img
+            <motion.img
               src={vectorRight}
-              className="logo-part right-part w-32 md:w-48 lg:w-64 absolute top-0 right-0 transition-all duration-1000"
+              className="logo-part right-part w-32 md:w-48 lg:w-64 absolute top-0 right-0"
               alt="Símbolo de Inovação - Parte Direita"
-              style={{
-                transform: animationFinished
-                  ? "translateX(-25%)"
-                  : "translateX(0)",
-              }}
+              initial={{ x: "50%" }}
+              animate={{ x: animationFinished ? "0%" : "50%" }}
+              transition={{ duration: 1, ease: "easeInOut" }}
             />
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
