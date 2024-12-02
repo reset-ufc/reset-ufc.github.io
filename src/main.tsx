95d309b07c9ba0abd586067396376a67cdcf4bcc
ArthurWillameBr <arthurwillame2005@gmail.com>
2024-12-02 08:42:07 -0300

    feat: add responsive Header component with navigation and dropdown menus

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import Header from './components/Header.tsx'
-import App from './App.tsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header/index.tsx";
+import App from "./App.tsx";
 import "./index.css";
-import Footer from './components/Footer/index.tsx'
-import { Helmet, HelmetProvider } from 'react-helmet-async'
+import Footer from "./components/Footer/index.tsx";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-    <HelmetProvider>
-      <Helmet titleTemplate='Reset | %s' />
-      <div className="pageContainer">
-        <Header />
-        <main className="content">
-          <App />
-        </main>
-        <Footer />
-      </div>
+      <HelmetProvider>
+        <Helmet titleTemplate="Reset | %s" />
+        <div className="pageContainer">
+          <Header />
+          <main className="content">
+            <App />
+          </main>
+          <Footer />
+        </div>
       </HelmetProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
