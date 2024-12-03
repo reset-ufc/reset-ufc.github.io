828e91c3c0c661fd54f3b2d6ca2096daf8d8a824
ArthurWillameBr <arthurwillame2005@gmail.com>
2024-12-03 19:31:55 -0300

    fix: adjust padding and spacing in CollaborativeResearch component for better layout

--- src/components/CollaborativeResearch/index.tsx
+++ src/components/CollaborativeResearch/index.tsx
@@ -7,7 +7,7 @@ export function CollaborativeResearch() {
       id="collaborative-research"
       className="py-12 md:py-16 lg:py-24 text-white relative overflow-hidden"
     >
-      <div className="container mx-auto px-4 relative z-10">
+      <div className="container mx-auto px-5  relative z-10">
         <div className="flex flex-col items-center lg:flex-row lg:justify-around lg:items-start gap-6 lg:gap-10">
           <motion.div
             className="flex flex-col justify-center items-center lg:items-start text-center lg:text-left max-w-md lg:max-w-lg"
@@ -15,7 +15,7 @@ export function CollaborativeResearch() {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6 }}
           >
-            <h2 className="text-2xl sm:text-3xl lg:text-4xl font-bold mb-4 leading-tight">
+            <h2 className="text-2xl sm:text-3xl lg:text-4xl font-bold  leading-tight">
               Somos um grupo de pesquisa{" "}
               <span className="text-[#ed6327]">colaborativa</span>
             </h2>
@@ -26,7 +26,7 @@ export function CollaborativeResearch() {
             </p>
           </motion.div>
           <motion.div
-            className="grid md:grid-cols-4 grid-cols-2 gap-4 md:gap-1 space-x-3 w-full max-w-lg lg:max-w-xl mt-8 lg:mt-0"
+            className="grid md:grid-cols-4 grid-cols-2 gap-4 md:gap-1 w-full max-w-lg lg:max-w-xl mt-8 lg:mt-0"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, delay: 0.2 }}
